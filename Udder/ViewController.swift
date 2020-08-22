d47dbc4c3c152901b7271b392fa3dc8e8de0dca4
Alex Huang <alexhuang169@hotmail.com>
2020-08-22 17:28:22 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Alex Huang on 2020-08-22.
+//  Copyright © 2020 Alex Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
